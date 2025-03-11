fc480b39a63fea8036e8d903367db1bd04d184d8
kvm1l03 <klabus03@gmail.com>
2025-03-11 10:17:59 +0100

    feat: Initialized frontend and backend

---
+++ backend/routes.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify(message="Welcome to the home page!")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
