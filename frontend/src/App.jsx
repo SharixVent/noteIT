cd581d0d9525bad0f681cc6d1cdf544560522c07
Dawid <sharixvent@gmail.com>
2025-03-28 10:42:01 +0100

    Added MainPage.jsx with styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home.jsx';
 import Login from './components/Login/Login.jsx';
 import Register from './components/Register/Register.jsx';
+import MainPage from './components/MainPage/MainPage.jsx';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/mainpage" element={<MainPage />} />
             </Routes>
         </Router>
     );
