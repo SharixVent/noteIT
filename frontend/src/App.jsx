2f6f96c3dfc3aa72ef3dce7b4f75f048e5b7cba8
kvm1l03 <klabus03@gmail.com>
2025-04-02 15:31:29 +0200

    feat: Integrated frontend with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/mainpage" element={<MainPage />} />
                 <Route path="/create" element={<Create />} />
+                <Route path='profile' element={<MainPage/>}/>
             </Routes>
         </Router>
     );
--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -1,44 +1,82 @@
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faNoteSticky, faUser} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faNoteSticky, faUser } from '@fortawesome/free-solid-svg-icons';
+import api from '../../api/data'; 
+import Note from '../Note/Note';
 import './MainPage.css';
 
-function MainPage()  {
+function MainPage() {
     const navigate = useNavigate();
+    const [notes, setNotes] = useState([]);
 
-    return <>
-        <div className="navbar-mp">
-            <div className="logo-mp">
-                <FontAwesomeIcon className='ikona-land' icon={faNoteSticky} /> 
-                <p>noteIT</p>
-            </div>
-            <div className="profile-mp" id='profile-mp'><FontAwesomeIcon icon={faUser} />example@email.com</div>
-        </div>
-        <div className="main-mp">
-            <div className="categories-mp">
-                <span>Categories</span>
-                <div className="container-cat-mp">
-                    <input type="checkbox" name="" id=""></input>
-                    <label htmlFor="">All</label>
-                    <input type="checkbox" name="" id=""></input>
-                    <label htmlFor="">Studies</label>
-                    <input type="checkbox" name="" id=""></input>
-                    <label htmlFor="">Work</label>
-                    <input type="checkbox" name="" id=""></input>
-                    <label htmlFor="">Others</label>
-                </div>
-                
-            </div>
-            <div className="container-mp">
-                <div className="notes-container-mp">
-                {/*Notatki tutaj*/}
-                </div>
-                <div className="cont-add-mp">
-                    <div className="add-note-mp"></div>
-                </div>
-            </div>
+    useEffect(() => {
+        // Fetch notes for the test user with ID 1
+        api.note.getUserNotes(1)
+            .then((fetchedNotes) => setNotes(fetchedNotes))
+            .catch((error) => console.error('Error fetching notes:', error));
+    }, []);
 
-        </div>
-    </>
+    const handleProfileClick = () => {
+        // Navigate to the profile page
+        navigate('/profile');
+    };
 
-} export default MainPage;
+    const handleAddNoteClick = () => {
+        // Navigate to the add note page
+        // TODO: Add add note functionality it requires a json that look like this:
+        // {"user_id": 1, "title": "My Second Note", "description": "This is the description of the note.", "color": "blue", "category": "Study"}
+        navigate('/add-note');
+    };
+
+    return (
+        <>
+            <div className="navbar-mp">
+                <div className="logo-mp">
+                    <FontAwesomeIcon className="ikona-land" icon={faNoteSticky} />
+                    <p>noteIT</p>
+                </div>
+                <div className="profile-mp" id="profile-mp" onClick={handleProfileClick}>
+                    <FontAwesomeIcon icon={faUser} />
+                    example@email.com
+                </div>
+            </div>
+            <div className="main-mp">
+                <div className="categories-mp">
+                    <span>Categories</span>
+                    <div className="container-cat-mp">
+                        <input type="checkbox" name="" id="" />
+                        <label htmlFor="">All</label>
+                        <input type="checkbox" name="" id="" />
+                        <label htmlFor="">Studies</label>
+                        <input type="checkbox" name="" id="" />
+                        <label htmlFor="">Work</label>
+                        <input type="checkbox" name="" id="" />
+                        <label htmlFor="">Others</label>
+                    </div>
+                </div>
+                <div className="container-mp">
+                    <div className="notes-container-mp">
+                        {/* Render notes here */}
+                        {notes.map((note) => (
+                            <Note
+                                key={note.id}
+                                title={note.title}
+                                description={note.description}
+                                color={note.color}
+                                category={note.category}
+                            />
+                        ))}
+                    </div>
+                    <div className="cont-add-mp">
+                        <div className="add-note-mp" onClick={handleAddNoteClick}>
+                            <p>+ Add Note</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MainPage;
