978f194e0768e783548bf088a8980eb20f8c25fb
KamilN27 <niezabitowskikamil@gmail.com>
2025-04-07 23:15:56 +0200

    Added register page

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,11 +1,48 @@
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import './Register.css'; // zmieniono z Login.css na Register.css
 
 function Register() {
-
     return (
         <>
-            <h1>Register Page</h1>
+            <div className='register-main'>
+
+                <div className="register-cork-board">    
+
+                    <div className="register-left-book">
+                        <div className="book"></div>
+                    </div>
+
+                    <div className="register-container">
+                        <div className="register-logo">
+                            <FontAwesomeIcon className='ikona-register' icon={faUserPlus} /> 
+                            <p>noteIT</p>
+                        </div>
+                        <form action="" className='register-form'>
+                            <label htmlFor="">Login</label>
+                            <input type="text" placeholder='Login ..'/>
+                            <label htmlFor="">Email</label>
+                            <input type="text" placeholder='Email ..'/>
+                            <label htmlFor="">Password</label>
+                            <input type="text" placeholder='Password ..'/>
+                            <label htmlFor="">Repeat Password</label>
+                            <input type="text" placeholder='Repeat Password ..'/>
+                            <input type="submit" value={"Register"}/>
+                        </form>
+
+                    </div>
+
+                    <div className="note-register">
+                        <div className="register-note-pin"></div>
+                        <div className="register-note-pinleg"></div>
+                        <p>Use a strong password to protect your notes!</p>
+                    </div>
+
+                    <div className="note-register-shadow"></div>
+                </div>
+            </div>
         </>
     )
 } 
-export default Register; 
+export default Register;
