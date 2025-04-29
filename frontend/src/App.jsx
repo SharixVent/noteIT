834dbb2a6a6c6695cf60676631c4185b9abf7fb1
Dawid <sharixvent@gmail.com>
2025-04-29 08:41:08 +0200

    Added NotePreview.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Login from './components/Login/Login.jsx';
 import Register from './components/Register/Register.jsx';
 import MainPage from './components/MainPage/MainPage.jsx';
 import Create from './components/Create/Create.jsx';
+import NotePrev from './components/NotesPreview/NotesPreview.jsx'
+import NoteDetails from './components/NotePreview/NotePreview.jsx';
 
 function App() {
     return (
@@ -15,6 +17,8 @@ function App() {
                 <Route path="/mainpage" element={<MainPage />} />
                 <Route path="/create" element={<Create />} />
                 <Route path='profile' element={<MainPage/>}/>
+                <Route path='/noteprev' element={<NotePrev/>}/>
+                <Route path="/notespreview/:noteId" element={<NoteDetails />} />
             </Routes>
         </Router>
     );
